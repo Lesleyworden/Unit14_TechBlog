cc820cbecc5683ec6b1c25da9eb86b5830beb4f0
Lesleyworden <70115227+Lesleyworden@users.noreply.github.com>
2021-01-12 14:17:26 -0800

    Create connection.js

---
+++ config/connection.js
@@ -0,0 +1,8 @@
+const Sequlize = require ('sequelize');
+require ('dotenv') .config();
+
+const sequelize = new Sequlize (
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+)
